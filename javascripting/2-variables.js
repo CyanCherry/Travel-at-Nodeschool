e90df4a5ad7b6e571c20e06faf92e74370568961
Cyan Cherry <861154915@qq.com>
2018-11-11 17:46:54 +0800

    update 2-variables.js

--- javascripting/2-variables.js
+++ javascripting/2-variables.js
@@ -1,7 +1,8 @@
 //JavaScript中定义变量时不需要也不能指定变量类型
 //变量类型由解释器根据变量值自动指定
 //一个变量可以赋予多种类型的值，未赋值时为undefined
-//定义一个String类型的变量
+
+//定义一个变量
 var variable = "some string";
 //在控制台输出这个变量
 console.log(variable);
