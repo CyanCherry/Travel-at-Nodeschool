bb973202a10c3e807dda6609372d6b89e796f672
Cyan Cherry <cyan_cherry@qq.com>
2019-02-07 19:58:03 +0800

    remove unnecessary files

--- sequelize/3-Promises.js
+++ sequelize/3-Promises.js
@@ -1,11 +1,14 @@
 const User = require('./2-Your first model');
 
-// User.create({firstName: 'Cyan', lastName: 'Cherry'});
-User.destroy({where: {firstName: 'John'}});
-User.findAll()
-    .then((dataSet) => {
-        dataSet.map((data) => {
-            console.log(data.dataValues)
-        })
+async function three() {
+    await User.create({firstName: 'Cyan', lastName: 'Cherry'});
+
+    let dataSet = await User.findAll();
+    dataSet.map((data) => {
+        console.log(data.dataValues)
     });
 
+    User.destroy({where: {firstName: 'Cyan'}});
+}
+
+three();
