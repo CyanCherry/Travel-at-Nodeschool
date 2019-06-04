a2b818ffb21b4b6cbfaf35811ee32725f15433e3
Cyan Cherry <cyan_cherry@qq.com>
2019-06-04 17:50:26 +0800

    Each arrow function without its own bindings to the this, arguments, super, or new.target keywords.

---
+++ functional-javascript-workshop/8-Basic Call.js
@@ -0,0 +1,20 @@
+// Truly name of this exercise is "Basic: Call"
+
+function duckCount() {
+    return Array.prototype.reduce.call(
+        arguments,
+        (count, argument) => Object.prototype.hasOwnProperty.call(argument, 'quack') ? count + 1 : count,
+        0
+    )
+}
+
+// duckCount = () => {
+//     return Array.prototype.reduce.call(
+//         arguments,
+//         (count, argument) => Object.prototype.hasOwnProperty.call(argument, 'quack') ? count + 1 : count,
+//         0
+//     )
+// }
+// don't make sense, because arrow function without its own bindings to the this, arguments, super, or new.target keywords.
+
+module.exports = duckCount;
